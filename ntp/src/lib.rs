d856b28a367cdbcf6c9fa709218820be62672216
David Ventura <davidventura27@gmail.com>
2024-02-27 19:54:48 +0100

    implement incomplete types to serialize an entire message

--- ntp/src/lib.rs
+++ ntp/src/lib.rs
@@ -3,9 +3,44 @@ use std::net::Ipv4Addr;
 
 const NTP_MESSAGE_LEN: usize = 48;
 
+struct Fix32 {
+    i: u16,
+    f: u16,
+}
+
+impl Serialize for Fix32 {
+    fn serialize(&self) -> Vec<u8> {
+        vec![
+            ((self.i & 0xff00) >> 8) as u8,
+            ((self.i & 0x00ff) >> 0) as u8,
+            ((self.f & 0xff00) >> 8) as u8,
+            ((self.f & 0x00ff) >> 0) as u8,
+        ]
+    }
+}
+// FIXME
+impl From<f32> for Fix32 {
+    fn from(f: f32) -> Fix32 {
+        Fix32 {
+            i: f.trunc() as u16,
+            f: f.fract() as u16,
+        }
+    }
+}
 struct NTPTimestamp {
     int_part: u32,
     frac_part: u32,
+}
+
+impl Serialize for NTPTimestamp {
+    fn serialize(&self) -> Vec<u8> {
+        let i = self.int_part.to_be_bytes();
+        let f = self.frac_part.to_be_bytes();
+        let mut ret = Vec::with_capacity(8);
+        ret.extend(i);
+        ret.extend(f);
+        ret
+    }
 }
 
 impl From<DateTime<Utc>> for NTPTimestamp {
@@ -120,9 +155,11 @@ pub struct NTPMessage {
     flags: NTPFlags,
     peer_stratum: u8,
     peer_polling_interval: u8,
+    /// eight signed bits indicates the precision of the clock in seconds, expressed as a power of 2:
+    /// -10 means 2^-10 == 1s/1024 == 0.97ms
     peer_clock_precision: u8,
-    root_delay: u32, // TODO,
-    root_dispersion: u32,
+    root_delay: Fix32,
+    root_dispersion: Fix32,
     ref_id: Reference,
     ref_tstamp: NTPTimestamp,
     origin_tstamp: NTPTimestamp,
@@ -137,7 +174,13 @@ impl NTPMessage {
         message.push(self.peer_stratum);
         message.push(self.peer_polling_interval);
         message.push(self.peer_clock_precision);
-        // FIXME
+        message.extend(self.root_delay.serialize());
+        message.extend(self.root_dispersion.serialize());
+        message.extend(self.ref_id.serialize());
+        message.extend(self.ref_tstamp.serialize());
+        message.extend(self.origin_tstamp.serialize());
+        message.extend(self.rcv_tstamp.serialize());
+        message.extend(self.transmit_tstamp.serialize());
         message
     }
 }
@@ -174,9 +217,9 @@ mod tests {
             },
             peer_stratum: 1,
             peer_polling_interval: 10,
-            peer_clock_precision: 0xf0, //0.000015,
-            root_delay: 0,              // 0.0,
-            root_dispersion: 0,         //0.000320,
+            peer_clock_precision: 0xf0,   //0.000015, TODO
+            root_delay: Fix32::from(0.0), // 0.0,
+            root_dispersion: Fix32::from(0.000320),
             ref_id: Reference::GPS,
             ref_tstamp: NTPTimestamp::from(ts),
             origin_tstamp: NTPTimestamp::from(ts),
@@ -184,6 +227,26 @@ mod tests {
             transmit_tstamp: NTPTimestamp::from(ts),
         };
 
-        assert_eq!(m.serialize(), vec![0x1a, 0x1, 0xa]);
+        #[rustfmt::skip]
+        assert_eq!(
+            m.serialize(),
+            vec![
+                0x1a, 0x1, 0xa, 0xf0,
+                /* root_delay*/
+                0, 0, 0, 0,
+                /* root_dispersion*/
+                0, 0, 0, 0,
+                /* ref id*/
+                b'G', b'P', b'S', 0,
+                /* ref tstamp */
+                197, 2, 4, 122, 0, 0, 0, 0,
+                /* origin tstamp */
+                197, 2, 4, 122, 0, 0, 0, 0,
+                /* rcv tstamp */
+                197, 2, 4, 122, 0, 0, 0, 0,
+                /* transmit tstamp */
+                197, 2, 4, 122, 0, 0, 0, 0,
+            ]
+        );
     }
 }
