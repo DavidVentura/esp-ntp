5ecfcba7aadbbf5d8a87494a5662c7856e5789c7
David Ventura <davidventura27@gmail.com>
2024-02-29 12:03:57 +0000

    return Alarm if server is not in sync

--- ntp/src/server.rs
+++ ntp/src/server.rs
@@ -2,45 +2,49 @@ use crate::proto::*;
 use chrono::{DateTime, NaiveDate, Utc};
 use std::time::Duration;
 pub struct GPSServer {
-    reftime: DateTime<Utc>,
+    reftime: Option<DateTime<Utc>>,
 }
 
 impl GPSServer {
     pub fn new() -> GPSServer {
-        let ntp_zero = DateTime::<Utc>::from_naive_utc_and_offset(
-            NaiveDate::from_ymd_opt(1900, 1, 1)
-                .unwrap()
-                .and_hms_opt(0, 0, 0)
-                .unwrap(),
-            Utc,
-        );
-        GPSServer { reftime: ntp_zero }
+        GPSServer { reftime: None }
     }
+
     pub fn update_reference_time(&mut self, dt: DateTime<Utc>) {
-        self.reftime = dt;
+        self.reftime = Some(dt);
     }
+
     pub fn answer_query(
         &self,
-        q: NTPMessage,
+        q: NTPQuery,
         received_at: DateTime<Utc>,
         now: DateTime<Utc>,
     ) -> NTPMessage {
         NTPMessage {
             flags: NTPFlags {
-                l: LeapIndicator::NoWarning,
+                l: match self.reftime {
+                    Some(_) => LeapIndicator::NoWarning,
+                    None => LeapIndicator::Alarm,
+                },
                 v: VersionNumber(NTP_VERSION),
-                m: Mode::SymPassive,
+                m: Mode::Server,
             },
             peer_stratum: 1,
-            peer_polling_interval: q.peer_polling_interval,
+            peer_polling_interval: 4, //q.peer_polling_interval,
             root_delay: Fix32 { i: 0, f: 0 },
             peer_clock_precision: PeerPrecision::from(Duration::from_micros(1)),
             root_dispersion: Fix32 { i: 0, f: 0 },
             ref_id: Reference::GPS,
-            ref_tstamp: NTPTimestamp::from(self.reftime),
+            ref_tstamp: NTPTimestamp::from(self.reftime.unwrap_or(ntp_zero())),
             origin_tstamp: q.transmit_tstamp,
             rcv_tstamp: NTPTimestamp::from(received_at),
             transmit_tstamp: NTPTimestamp::from(now),
         }
+    }
+}
+
+impl Default for GPSServer {
+    fn default() -> Self {
+        Self::new()
     }
 }
