d15f8a2f8a803bba49dfec56442e8546a55aead6
David Ventura <davidventura27@gmail.com>
2024-02-29 19:35:50 +0000

    implement basic server

--- ntp/src/bin.rs
+++ ntp/src/bin.rs
@@ -2,24 +2,38 @@ use chrono::{offset::Local, Utc};
 use ntp::proto::*;
 use ntp::server::GPSServer;
 use std::net::UdpSocket;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 fn main() -> std::io::Result<()> {
-    let now = Local::now().with_timezone(&Utc);
-    let s = GPSServer::new();
+    let s = Arc::new(Mutex::new(GPSServer::new()));
+    let s2 = s.clone();
 
-    let socket = UdpSocket::bind("127.0.0.1:123")?;
-    let mut buf = [0; 512];
-    let (amt, src) = socket.recv_from(&mut buf)?;
-    if amt != NTP_MESSAGE_LEN {
-        println!("baaad");
-        return Ok(());
+    thread::spawn(move || handle_ntp_queries(s2));
+    loop {
+        let now = Local::now().with_timezone(&Utc);
+        s.lock().unwrap().update_reference_time(now);
+        std::thread::sleep(Duration::from_millis(1000));
     }
-    println!("pkt, {:?}", src);
-    let buf = &mut buf[..NTP_MESSAGE_LEN];
-    let q = NTPQuery::deserialize(buf).unwrap();
-    let a = s.answer_query(q, now, now);
+}
 
-    let outbuf = a.serialize();
-    socket.send_to(&outbuf, src)?;
-    Ok(())
+fn handle_ntp_queries(s: Arc<Mutex<GPSServer>>) -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:123")?;
+    loop {
+        let mut buf = [0; 128];
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        if amt != NTP_MESSAGE_LEN {
+            println!("bad, len was {}: {:?}", amt, &buf);
+            continue;
+        }
+        let now = Local::now().with_timezone(&Utc);
+        println!("pkt from {:?}", src);
+        let buf = &mut buf[..NTP_MESSAGE_LEN];
+        let q = NTPQuery::deserialize(buf).unwrap();
+        let a = s.lock().unwrap().answer_query(q, now, now);
+
+        let outbuf = a.serialize();
+        socket.send_to(&outbuf, src)?;
+    }
 }
