ddc3e623c8b8eb93bea0c8d3c2cc87141f8db240
David Ventura <davidventura27@gmail.com>
2024-03-05 09:56:59 +0100

    add port config serialization

--- ubx/src/proto.rs
+++ ubx/src/proto.rs
@@ -1,7 +1,118 @@
-use std::collections::VecDeque;
-
 use chrono::{DateTime, NaiveDate, TimeDelta, Utc};
+use std::collections::VecDeque;
+use std::time::Duration;
 
+trait Serialize {
+    fn serialize(&self) -> Vec<u8>;
+}
+#[derive(Debug)]
+pub enum CfgPacket {
+    Msg(CfgMsg),
+    Port(Port),
+}
+
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum PortProto {
+    UBX,
+    NMEA,
+    UBX_NMEA,
+}
+#[derive(Debug)]
+pub enum PortMode {
+    UART(UartCfg),
+}
+impl PortMode {}
+#[derive(Debug)]
+pub enum UartMode {
+    Mode8N1,
+}
+#[derive(Debug)]
+pub struct UartCfg {
+    pub baudrate: u32,
+    pub mode: UartMode,
+    pub lsb: bool,
+}
+#[derive(Debug)]
+pub struct Port {
+    /*
+    2, 16bit le bitflags (00 01; 00 01), first is PROTO IN, second is PROTO OUT
+    B5 62
+    06 00
+    14 00
+    04 00 00 00 00 32 00 00 00 00 00 00 01 00 01 00 00 00 00 00
+    52 94
+
+    00 = None
+    01 = UBX
+    02 = NMEA
+    03 = NMEA + UBX
+    (others)
+
+    04 = SPI
+    uart in 9600 8N1, ubx-ubx, lsb
+    B5 62
+    06 00
+    14 00
+    01 00 00 00 D0 08 00 00 80 25 00 00 01 00 01 00 00 00 00 00
+    ^^^^^ serial
+                ^^^^^ 8N1
+                      ^^ LSB (MSB=1)
+                            ^^^^^^^^^^^ baudrate
+                                        ^^^^^^^^^^^ in/out
+    9A 79
+    */
+    pub port_mode: PortMode,
+    pub proto_in: PortProto,
+    pub proto_out: PortProto,
+}
+
+impl Serialize for PortProto {
+    fn serialize(&self) -> Vec<u8> {
+        let mut out = Vec::new();
+        let id: u16 = match self {
+            PortProto::UBX => 1,
+            PortProto::NMEA => 2,
+            PortProto::UBX_NMEA => 3,
+        };
+        out.extend(u16::to_le_bytes(id));
+        out
+    }
+}
+impl Serialize for UartCfg {
+    fn serialize(&self) -> Vec<u8> {
+        let mut out = Vec::new();
+        out.extend(u32::to_le_bytes(1)); // 1 = UART
+        out.extend(match self.mode {
+            UartMode::Mode8N1 => vec![0xD0, 0x08],
+        });
+        out.extend(match self.lsb {
+            true => u16::to_le_bytes(0),
+            false => u16::to_le_bytes(1),
+        });
+        out.extend(u32::to_le_bytes(self.baudrate));
+        out
+    }
+}
+impl Serialize for PortMode {
+    fn serialize(&self) -> Vec<u8> {
+        let mut out = Vec::new();
+        out.extend(match self {
+            PortMode::UART(cfg) => cfg.serialize(),
+        });
+        out
+    }
+}
+impl Serialize for Port {
+    fn serialize(&self) -> Vec<u8> {
+        let mut out = Vec::new();
+        out.extend(self.port_mode.serialize());
+        out.extend(self.proto_in.serialize());
+        out.extend(self.proto_out.serialize());
+        out.extend(vec![0, 0, 0, 0]);
+        out
+    }
+}
 #[derive(Debug)]
 pub enum NavPacket {
     TimeUTC(TimeUTC),
@@ -18,7 +129,7 @@ impl From<Packet> for NavPacket {
 }
 
 impl TimeGPS {
-    pub fn serialize_request(&self) -> Vec<u8> {
+    pub fn serialize_request() -> Vec<u8> {
         let p = Packet {
             class: Class::Navigation,
             id: 0x20,
@@ -29,15 +140,41 @@ impl TimeGPS {
 }
 
 #[derive(Debug)]
+pub struct CfgMsg {
+    pub c: Class,
+    pub id: u8,
+    pub rate: Duration,
+}
+
+impl CfgMsg {
+    pub fn serialize_request(&self) -> Vec<u8> {
+        let p = Packet {
+            class: Class::ConfigInput,
+            id: self.id,
+            //B5 62 hdr
+            //06 08 class id
+            //06 00 len
+            //E8 03 01 00 01 00 payload
+            //01 39 ck
+            payload: vec![0xe8, 0x03, 0x1, 0x0, 0x1, 0x0],
+        };
+        p.serialize()
+    }
+}
+
+#[derive(Debug)]
 pub enum ParsedPacket {
     Navigation(NavPacket),
+    Configuration(CfgPacket),
+    Nack,
 }
 
 impl From<Packet> for ParsedPacket {
     fn from(p: Packet) -> ParsedPacket {
         match p.class {
             Class::Navigation => ParsedPacket::Navigation(NavPacket::from(p)),
-            _ => panic!(),
+            Class::AckNack => ParsedPacket::Nack,
+            _ => panic!("what do {:?}", p),
         }
     }
 }
@@ -113,7 +250,7 @@ pub struct TimeGPS {
     pub week: i16,
     pub leap_sec: i8,
     pub valid_flags: Valid,
-    pub accuracy: u32,
+    pub accuracy_nanos: u32,
 }
 
 fn buf_to_2u8(buf: &[u8]) -> [u8; 2] {
@@ -131,13 +268,14 @@ impl From<&[u8]> for TimeGPS {
             week: i16::from_le_bytes(buf_to_2u8(&buf[8..10])),
             leap_sec: buf[10] as i8,
             valid_flags: Valid::from(buf[11]),
-            accuracy: u32::from_le_bytes(buf_to_4u8(&buf[12..])),
+            accuracy_nanos: u32::from_le_bytes(buf_to_4u8(&buf[12..])),
         }
     }
 }
 impl From<TimeGPS> for Option<DateTime<Utc>> {
     fn from(t: TimeGPS) -> Option<DateTime<Utc>> {
-        if t.accuracy > 30 {
+        if t.accuracy_nanos > 1_000_000_000 {
+            // 1s?
             return None;
         }
         // https://www.gps.gov/technical/icwg/IS-GPS-200G.pdf, page 39
@@ -370,6 +508,7 @@ impl<I: Iterator<Item = u8>> Iterator for PacketIterator<I> {
                 Ok(p) => {
                     self.consecutive_inc = 0;
                     drop(self.buf.drain(..(p.len_with_frame())));
+                    println!("local buf size {}", self.buf.len());
                     return Some(p);
                 }
             }
@@ -412,7 +551,7 @@ mod tests {
         match pp {
             ParsedPacket::Navigation(n) => match n {
                 NavPacket::TimeGPS(t) => {
-                    assert_eq!(t.accuracy, 1);
+                    assert_eq!(t.accuracy_nanos, 1);
                     // converted from gps week + gps seconds of week with
                     // https://www.labsat.co.uk/index.php/en/gps-time-calculator
                     // 2016-10-30T19:46:24.997659144Z
@@ -429,6 +568,7 @@ mod tests {
                     println!("UTC {:?}", DateTime::<Utc>::from(t));
                 }
             },
+            _ => panic!(),
         }
     }
     #[test]
@@ -493,5 +633,24 @@ mod tests {
             count += 1;
         }
         assert_eq!(count, 0);
+    }
+
+    #[test]
+    fn portconfig_serialization() {
+        let pc = Port {
+            port_mode: PortMode::UART(UartCfg {
+                baudrate: 9600,
+                mode: UartMode::Mode8N1,
+                lsb: true,
+            }),
+            proto_in: PortProto::UBX,
+            proto_out: PortProto::UBX,
+        };
+
+        let expected = vec![
+            0x01, 0x00, 0x00, 0x00, 0xD0, 0x08, 0x00, 0x00, 0x80, 0x25, 0x00, 0x00, 0x01, 0x00,
+            0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+        assert_eq!(pc.serialize(), expected);
     }
 }
