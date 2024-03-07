d9edfe870acc2d123f64e0af8767541dc45817e5
David Ventura <davidventura27@gmail.com>
2024-03-07 11:02:10 +0100

    move metrics endpoint to /metrics

--- esp/src/http.rs
+++ esp/src/http.rs
@@ -8,8 +8,7 @@ use crate::metrics::Metrics;
 pub(crate) fn server(m: Arc<Mutex<Metrics>>) -> Result<EspHttpServer<'static>, EspIOError> {
     let mut httpserver = EspHttpServer::new(&Default::default())?;
 
-    httpserver.fn_handler("/", Method::Get, move |req| {
-        // Can't get `req.content_len()` to work, the Headers trait doesnt seem to work
+    httpserver.fn_handler("/metrics", Method::Get, move |req| {
         let mut resp = req.into_response(200, None, &[("content-type", "text/plain")])?;
 
         for ln in m.lock().unwrap().serialize() {
